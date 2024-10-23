c551e153d3f7b4f3330bcfd855c517ebcddfb85b
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2024-10-22 22:22:40 -0500

    cambiando proceso de snyk

--- .github/workflows/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin $(aws sts get-caller-identity --query Account --output text).dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
-docker build -t $(aws sts get-caller-identity --query Account --output text).dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${REPOSITORY_NAME}:${GIT_TAG} .
-docker push $(aws sts get-caller-identity --query Account --output text).dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${REPOSITORY_NAME}:${GIT_TAG}
